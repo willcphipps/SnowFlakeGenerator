593440458c8f0c46a9d54e055595d79c99ba79b7
William Phipps <willcphipps@gmail.com>
2020-08-19 13:13:20 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById('canvas-1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const maxLevel = 5;
+const branches = 3;
+const sides = Math.floor((Math.random() * 10) + 3)
+const spread = (Math.random() * 48) + 0.51;
+
+ctx.translate(canvas.width / 2, canvas.height / 2);
+
+const angle = Math.PI * 2 * spread;
+
+function drawLine(level) {
+    if (level > maxLevel) return;
+    console.log("this is the level...",level)
+    
+    ctx.strokeStyle = "#fff";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(0,0)
+    ctx.lineTo(200, 0);
+    ctx.stroke();
+
+    for (let i = 1; i < branches + 1; i++){
+        console.log("in the for loop")
+        ctx.save();
+        ctx.translate(200 * i / (branches + 1), 0);
+        ctx.scale(0.5, 0.5)
+        ctx.save();
+
+        ctx.rotate(angle);
+        drawLine(level + 1);
+        ctx.restore();
+        ctx.save();
+
+        ctx.rotate(-angle);
+        drawLine(level + 1);
+        ctx.restore();
+
+        ctx.restore();
+    }
+}
+
+for (let i = 0; i < sides; i++){
+    drawLine(0);
+    ctx.rotate(Math.PI * 2 / sides)
+}
